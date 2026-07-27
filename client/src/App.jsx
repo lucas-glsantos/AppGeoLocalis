8c5896da56abbff7129a2ce3324f184d2f090575
lucas-glsantos <glsantos-dev@protonmail.com>
2026-07-27 01:06:58 -0300

    fix: update main config and packages 🚀

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,26 +3,26 @@ import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "react-hot-toast";
 
-import { useApp } from "./controllers/AppContext";
-import LoadingScreen from "./components/shared/loader/LoadingScreen";
+import { useApp } from "@/controllers/AppContext";
+import LoadingScreen from "@/components/shared/loader/LoadingScreen";
 
 // Rotas públicas
-const HomePage = lazy(() => import("./pages/HomePage"));
-const PostPage = lazy(() => import("./components/shared/PostPage"));
-const Login = lazy(() => import("./pages/auth/Login"));
-const Nearby = lazy(() => import("./pages/NearbyBusinesPage"));
+const HomePage = lazy(() => import("@/pages/HomePage"));
+const PostPage = lazy(() => import("@/components/shared/PostPage"));
+const Login = lazy(() => import("@/pages/auth/Login"));
+const Nearby = lazy(() => import("@/pages/NearbyBusinesPage"));
 
 // Rotas do autor
-const Layout = lazy(() => import("./components/layout/Layout"));
-const Dashboard = lazy(() => import("./components/layout/Dashboard"));
-const UserAddPost = lazy(() => import("./pages/dashboard/customer/UserAddPost"));
-const UserListPost = lazy(() => import("./pages/dashboard/customer/UserListPost"));
-const UserEditPost = lazy(() => import("./pages/dashboard/customer/UserEditPost"));
-const Comments = lazy(() => import("./pages/dashboard/comments/Comments"));
-const UserAddBusiness = lazy(() => import("./pages/dashboard/busines/UserAddBusiness"));
-const UserListBusiness = lazy(() => import("./pages/dashboard/busines/UserListBusiness"));
-const UserEditBusiness = lazy(() => import("./pages/dashboard/busines/UserEditBusiness"));
-const Metrics = lazy(() => import("./pages/dashboard/metrics/Metrics"));
+const Layout = lazy(() => import("@/components/layout/Layout"));
+const Dashboard = lazy(() => import("@/components/layout/Dashboard"));
+const UserAddPost = lazy(() => import("@/pages/dashboard/customer/AddPost"));
+const UserListPost = lazy(() => import("@/pages/dashboard/customer/ListPost"));
+const UserEditPost = lazy(() => import("@/pages/dashboard/customer/EditPost"));
+const Comments = lazy(() => import("@/pages/dashboard/comments/Comments"));
+const UserAddBusiness = lazy(() => import("@/pages/dashboard/busines/UserAddBusiness"));
+const UserListBusiness = lazy(() => import("@/pages/dashboard/busines/UserListBusiness"));
+const UserEditBusiness = lazy(() => import("@/pages/dashboard/busines/UserEditBusiness"));
+const Metrics = lazy(() => import("@/pages/dashboard/metrics/Metrics"));
 
 const ProtectedRoute = memo(() => {
 	const { isAuthenticated, isLoading } = useApp();
