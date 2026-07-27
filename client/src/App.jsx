fa707b2e62ced152aeb1ea1c29e2462af437b0f8
lucas-glsantos <glsantos-dev@protonmail.com>
2026-07-27 00:19:51 -0300

    fix: Correção de erro de importação no App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import { useApp } from "./controllers/AppContext";
 import LoadingScreen from "./components/shared/loader/LoadingScreen";
 
 // Rotas públicas
-const Home = lazy(() => import("./pages/HomePage"));
+const HomePage = lazy(() => import("./pages/HomePage"));
 const PostPage = lazy(() => import("./components/shared/PostPage"));
 const Login = lazy(() => import("./pages/auth/Login"));
 const Nearby = lazy(() => import("./pages/NearbyBusinesPage"));
@@ -15,13 +15,13 @@ const Nearby = lazy(() => import("./pages/NearbyBusinesPage"));
 // Rotas do autor
 const Layout = lazy(() => import("./components/layout/Layout"));
 const Dashboard = lazy(() => import("./components/layout/Dashboard"));
-const AddPost = lazy(() => import("./pages/dashboard/customer/UserAddPost"));
-const ListPost = lazy(() => import("./pages/dashboard/customer/UserListPost"));
-const EditPost = lazy(() => import("./pages/dashboard/customer/UserEditPost"));
+const UserAddPost = lazy(() => import("./pages/dashboard/customer/UserAddPost"));
+const UserListPost = lazy(() => import("./pages/dashboard/customer/UserListPost"));
+const UserEditPost = lazy(() => import("./pages/dashboard/customer/UserEditPost"));
 const Comments = lazy(() => import("./pages/dashboard/comments/Comments"));
-const AddBusiness = lazy(() => import("./pages/dashboard/busines/UserAddBusiness"));
-const ListBusiness = lazy(() => import("./pages/dashboard/busines/UserListBusiness"));
-const EditBusiness = lazy(() => import("./pages/dashboard/busines/UserEditBusiness"));
+const UserAddBusiness = lazy(() => import("./pages/dashboard/busines/UserAddBusiness"));
+const UserListBusiness = lazy(() => import("./pages/dashboard/busines/UserListBusiness"));
+const UserEditBusiness = lazy(() => import("./pages/dashboard/busines/UserEditBusiness"));
 const Metrics = lazy(() => import("./pages/dashboard/metrics/Metrics"));
 
 const ProtectedRoute = memo(() => {
@@ -37,7 +37,7 @@ const App = () => {
 			<Toaster position="top-center" toastOptions={{ duration: 5000 }} />
 			<Suspense fallback={<LoadingScreen />}>
 				<Routes>
-					<Route path="/" element={<Home />} />
+					<Route path="/" element={<HomePage />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/post/:id" element={<PostPage />} />
 					<Route path="/nearby" element={<Nearby />} />
@@ -45,13 +45,13 @@ const App = () => {
 					<Route element={<ProtectedRoute />}>
 						<Route path="/dashboard" element={<Layout />}>
 							<Route index element={<Dashboard />} />
-							<Route path="add-post" element={<AddPost />} />
-							<Route path="list-post" element={<ListPost />} />
-							<Route path="edit-post/:postId" element={<EditPost />} />
+							<Route path="add-post" element={<UserAddPost />} />
+							<Route path="list-post" element={<UserListPost />} />
+							<Route path="edit-post/:postId" element={<UserEditPost />} />
 							<Route path="list-comment" element={<Comments />} />
-							<Route path="add-business" element={<AddBusiness />} />
-							<Route path="list-business" element={<ListBusiness />} />
-							<Route path="edit-business/:businessId" element={<EditBusiness />} />
+							<Route path="add-business" element={<UserAddBusiness />} />
+							<Route path="list-business" element={<UserListBusiness />} />
+							<Route path="edit-business/:businessId" element={<UserEditBusiness />} />
 							<Route path="metrics" element={<Metrics />} />
 						</Route>
 					</Route>
