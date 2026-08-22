dd0c478fcb6f2612b271b07f182fc10fb9e214a6
lucas-glsantos <glsantos-dev@protonmail.com>
2026-08-21 22:25:18 -0300

    fix: remove bug in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,28 +15,6 @@ export default defineConfig({
 	build: {
 		// Gerá código nativo e moderno, menor e mais rápido de ser lido
 		target: 'es2022',
-		minify: 'esbuild',
-
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					// Verifica se o arquivo vem da pasta node_modules
-					if (id.includes('node_modules')) {
-						// Garante que o react fique em cache no navegador
-						if (id.includes('react') || id.includes('react-dom')) {
-							return 'vendor-react';
-						}
-
-						// Utilitários grandes
-						if (id.includes('react-router') || id.includes('axios')) {
-							return 'vendor-utils';
-						}
-
-						// Fallback para qualquer outra biblioteca instalada
-						return 'vendor-shared'; 
-					}
-				}
-			}
-		}
+		minify: 'esbuild'
 	}
 });
